d1b28ef677bc9e29ef279cb274ac23ee11eec5ff
Juan Tarrel <juantarrel@gmail.com>
2019-01-10 15:07:20 -0800

    scripts

---
+++ deployment-script.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+# Deploy Script
+
+####################################
+#
+# Configuration vars
+#
+####################################
+
+# user
+AWS_USER=
+# enviroments
+AWS_QA=
+AWS_STAGE=
+AWS_PRODUCTION=
+# cert .pem
+AWS_CERT=
+# name project
+NAME_PROJECT=
+# repository
+GIT_REPOSITORY="https://user:password@bitbucket.org/userowner/$NAME_PROJECT.git"
+ENVIROMENT=0
+# remote commands @TODO its works but need refactor
+REMOTE_COMMANDS=`cat remote-commands.sh | awk '{print $0, "\nif [[ $? -gt 0 ]]; then\n echo \"\033[0;31mThe is an error\033[m\"\nreturn 1\n fi"}'`
+
+BRANCH_TO_DEPLOY=""
+echo "What enviroment do you want to deploy?"
+echo "-- Select Number --"
+echo -e "QA \t\t(1)"
+echo -e "STAGE \t\t(2)"
+# echo -e "PRODUCTION \t(3)"
+# echo -e "EXIT \t\t(0)"
+read ENVIROMENT
+
+function listBranches {
+    echo "Listing branches..."
+    if ! [ -d $NAME_PROJECT ]; then
+        git clone $GIT_REPOSITORY &>/dev/null
+    fi
+
+    cd $NAME_PROJECT
+    git pull>/dev/null
+    git branch -av | awk 'BEGIN {FS=" "} {gsub("*|refs/heads/|remotes/origin/", "")} {print $1}' > branches_clean.txt
+    sort -u branches_clean.txt > branches_uniques.txt
+    readarray BRANCHES_ARRAY < branches_uniques.txt
+
+    # for branches array
+    echo "-- Select branch to deploy to $1 --"
+    for (( i=1; i<${#BRANCHES_ARRAY[@]}+1; i++ ));
+    do
+      echo -e ${BRANCHES_ARRAY[$i-1]} "\t\t" "($i)"
+    done
+    read SELECTED_ARRAY_BRANCH
+    BRANCH_TO_DEPLOY=${BRANCHES_ARRAY[SELECTED_ARRAY_BRANCH-1]}
+    cd ..
+    rm -rf $NAME_PROJECT
+}
+
+function deploy {
+    local ENV=""
+    local ENV_NAME=""
+    case $1 in
+        1 )
+            ENV=$AWS_QA
+            ENV_NAME="QA"
+            ;;
+        2 )
+            ENV=$AWS_STAGE
+            ENV_NAME="STAGE"
+            ;;
+        3 )
+            ENV=$AWS_PRODUCTION
+            ENV_NAME="PRODUCTION"
+            ;;
+        * )
+        exit 0
+        ;;
+    esac
+
+    listBranches $ENV_NAME
+
+    echo "Connecting to $ENV_NAME..."
+    ssh -i $AWS_CERT -T $AWS_USER@$ENV << EOSSH
+        echo $REMOTE_COMMANDS
+        makeBackup $NAME_PROJECT
+        pullProject $NAME_PROJECT $GIT_REPOSITORY $BRANCH_TO_DEPLOY
+EOSSH
+
+    if [[ $? -eq 0 ]]; then
+        echo -e "\033[0;32mThe enviroment $ENV_NAME is done !!!  check here -> http://$ENV\033[m"
+        return 0
+    fi
+
+}
+
+deploy $ENVIROMENT
+
---
+++ remote-commands.sh
@@ -0,0 +1,77 @@
+
+function makeBackup {
+
+    NAME_PROJECT=$1
+
+    if ! [ -d "~/Projects/$NAME_PROJECT" ]; then
+        return 0;
+    fi
+
+    echo -e "\033[0;32mConnected\033[m"
+    if ! [ -d "Backups" ]; then
+        mkdir Backups
+        echo -e "\033[0;32mBackups folder created\033[m"
+    fi
+
+    echo "Creating backup for current project..."
+
+    cp -rf ~/Projects/$NAME_PROJECT Backups
+
+    cd Backups
+
+    if [ -d "$NAME_PROJECT/vendor" ]; then
+        rm -rf $NAME_PROJECT/vendor
+        echo -e "\033[0;32mVendor deleted\033[m"
+    fi
+
+    if [ -d "$NAME_PROJECT/node_modules" ]; then
+        rm -rf $NAME_PROJECT/node_modules
+        echo -e "\033[0;32mNode modules deleted\033[m"
+    fi
+
+    tar -zcvf `echo "$(date "+%Y-%m-%d-%H-%M-%S").tar.gz"` `echo "$NAME_PROJECT"`>/dev/null
+    rm -rf $NAME_PROJECT
+    echo -e "\033[0;32mBackup created\033[m"
+
+    if [[ $? -gt 0 ]]; then
+        echo "The is an error"
+        return 1
+    fi
+}
+
+function pullProject {
+    echo "Pulling branch to project..."
+
+    if [ -d ~/Projects/$1 ]; then
+        if [ -f ~/Projects/$1/.env ]; then
+            cp ~/Projects/$1/.env ~/Projects/
+        fi
+        rm -r ~/Projects/$1
+    fi
+
+    echo "Cloning repository"
+    cd ~/Projects
+    git clone $2
+    echo -e "\033[0;32mRepository cloned\033[m"
+
+    cd $1
+
+    git checkout $3
+
+    if [ -f ~/Projects/.env ]; then
+        cp ~/Projects/.env ~/Projects/$1/
+    fi
+
+    echo "Removing git files..."
+    sudo rm -rf .git*
+
+    echo "Running migrations..."
+    cd Loyalty.Database/
+    dotnet run 
+
+    echo "Creating binary"
+    dotnet publish -o awssite
+
+    sudo cp -vnpr awssite ../:x!
+
+}
